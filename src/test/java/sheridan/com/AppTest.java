73dae32d165b6c6fb9d4eae0cb6b9e069a4af501
CHJ2000 <jeffcart@sheridancollege.ca>
2025-03-25 16:54:47 -0400

    loadTime tests added

--- src/test/java/sheridan/com/AppTest.java
+++ src/test/java/sheridan/com/AppTest.java
@@ -1,6 +1,7 @@
 package sheridan.com;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
@@ -15,9 +16,28 @@ public class AppTest
    public void testGoogleSearch(){
     WebDriverManager.chromedriver().setup();
     WebDriver driver = new ChromeDriver();
-    driver.get("https://www.google.com");
 
+    driver.get("https://www.google.com");
     assertEquals("Google", driver.getTitle());
+    driver.get("https://www.google.com/news");
+    assertEquals("Google News", driver.getTitle());
+    driver.get("https://www.google.com/store");
+    assertEquals("Google Store for Google Made Devices & Accessories", driver.getTitle());
     driver.quit();
+   }
+
+   @Test 
+   public void testLoadTime(){
+      WebDriverManager.chromedriver().setup();
+      WebDriver driver = new ChromeDriver();
+
+      long startTime = System.currentTimeMillis();
+      driver.get("https://www.google.com");
+      long endTime = System.currentTimeMillis();
+
+      long loadTime = endTime - startTime;
+      System.out.println("Time to load was " + loadTime + "milliseconds");
+      assertTrue(loadTime < 1000);
+      driver.quit();
    }
 }
